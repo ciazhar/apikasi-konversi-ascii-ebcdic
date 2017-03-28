13135fdb19c18cb5b0f8edcfd159063440a4359f
ciazhar <daisetsunakama@gmail.com>
2017-03-28 15:35:35 +0700

    initial commit

---
+++ src/ascii2ebcdic/Ascii2ebcdic.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ascii2ebcdic;
+
+/**
+ *
+ * @author ciazhar
+ */
+public class Ascii2ebcdic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ascii2ebcdicform form = new Ascii2ebcdicform();
+        form.setTitle("ASCII2EBCDIC");///judul
+        form.setLocationRelativeTo(null);///set posisi frame. null berarti ada di center
+        form.setVisible(true);///membuat f menjadi kelihatan
+        
+        
+    }
+    
+}
